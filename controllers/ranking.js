95f0d6da51696e8eb85c0f559d2d868dd58de77c
TaeHyeonKim <33569961+socm9145@users.noreply.github.com>
2021-06-01 19:24:48 +0900

    ranking& status updated

--- controllers/ranking.js
+++ controllers/ranking.js
@@ -1,5 +1,10 @@
 const config = require('../testConfig');
+const History = require('../models/History.js');
+const User = require('../models/User.js');
 
-module.exports = (req,res) => {
-    res.render(config.viewPath+'rankingView.ejs');
+module.exports = async (req,res) => {
+    const histories = await History.find({}).sort({score: 'desc'});
+    const users = await User.find({});
+
+    res.render(config.viewPath+'rankingView.ejs', {users, histories});
 }
--- controllers/status.js
+++ controllers/status.js
@@ -1,5 +1,10 @@
 const config = require('../testConfig');
+const User = require('../models/User.js');
+const History = require('../models/History.js');
 
-module.exports = (req,res) => {
-    res.render(config.viewPath+'statusView.ejs');
+module.exports = async (req,res) => {
+    const users = await User.find({username: req.session.userid});
+    const histories = await History.find({player1: req.session.userid}).sort({Date: 'desc'});
+
+    res.render(config.viewPath+'statusView.ejs', {users, histories});
 }
