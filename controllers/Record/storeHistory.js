519a85be3bed0bd7b75c02501b9d2f8448074b77
TaeHyeonKim <33569961+socm9145@users.noreply.github.com>
2021-06-10 19:42:27 +0900

    updated

--- controllers/Record/storeHistory.js
+++ controllers/Record/storeHistory.js
@@ -4,7 +4,7 @@ const User = require('../../models/User.js')
 module.exports = async (req, res) => {
     const {winner, loser} = req.body;
 
-    History.create(req.body, (error, history) => {
+    await History.create(req.body, (error, history) => {
         if (error) {
             return res.redirect('/history')
         }
@@ -14,21 +14,34 @@ module.exports = async (req, res) => {
     let winners = await User.findOne({username: winner});
 
     let total = {total: winners.total + 1};
+    console.log("1." + total);
     let win = {win: winners.win + 1};
+    console.log("2." + win);
     let rate = {rate: ((winners.win / winners.total) * 100).toFixed(0)}
+    console.log("3." + rate);
 
     await User.findOneAndUpdate({username: winner}, total);
     await User.findOneAndUpdate({username: winner}, win);
+
+    console.log("win updated");
     await User.findOneAndUpdate({username: winner}, rate);
+    
+    console.log("winner updated");
 
     let losers = await User.findOne({username: loser});
 
+    console.log("loser inserted");
+
     total = {total: losers.total + 1};
+    console.log("4." + total);
     let lose = {lose: losers.lose + 1};
+    console.log("5." + lose);
     rate = {rate: ((losers.win / losers.total) * 100).toFixed(0)}
+    console.log("6." + rate);
 
     await User.findOneAndUpdate({username: loser}, total);
     await User.findOneAndUpdate({username: loser}, lose);
     await User.findOneAndUpdate({username: loser}, rate);
 
+    console.log("loser updated")
 }
