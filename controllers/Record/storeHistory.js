02e74bbd67c457d4515684f55bfa0de587d47f4d
TaeHyeonKim <33569961+socm9145@users.noreply.github.com>
2021-06-10 20:02:03 +0900

    Update storeHistory.js

--- controllers/Record/storeHistory.js
+++ controllers/Record/storeHistory.js
@@ -14,34 +14,20 @@ module.exports = async (req, res) => {
     let winners = await User.findOne({username: winner});
 
     let total = {total: winners.total + 1};
-    console.log("1." + total);
     let win = {win: winners.win + 1};
-    console.log("2." + win);
-    let rate = {rate: ((winners.win / winners.total) * 100).toFixed(0)}
-    console.log("3." + rate);
+    let rate = {rate: ((win.win / total.total) * 100).toFixed(0)}
 
     await User.findOneAndUpdate({username: winner}, total);
     await User.findOneAndUpdate({username: winner}, win);
-
-    console.log("win updated");
     await User.findOneAndUpdate({username: winner}, rate);
-    
-    console.log("winner updated");
 
     let losers = await User.findOne({username: loser});
 
-    console.log("loser inserted");
-
     total = {total: losers.total + 1};
-    console.log("4." + total);
     let lose = {lose: losers.lose + 1};
-    console.log("5." + lose);
-    rate = {rate: ((losers.win / losers.total) * 100).toFixed(0)}
-    console.log("6." + rate);
+    rate = {rate: ((losers.win / total.total) * 100).toFixed(0)}
 
     await User.findOneAndUpdate({username: loser}, total);
     await User.findOneAndUpdate({username: loser}, lose);
     await User.findOneAndUpdate({username: loser}, rate);
-
-    console.log("loser updated")
 }
